c119bb1ce8fb9c29db8f68e50745e99c2285a5e6
Micalhl <micalhl@sina.cn>
2024-03-08 21:11:56 +0800

    Update TabooLib, Improve ObjectList, Fix BlastMining

--- project/module-custom-splendid/src/main/kotlin/com/mcstarrysky/aiyatsbus/module/custom/splendid/mechanism/entry/object/ObjectString.kt
+++ project/module-custom-splendid/src/main/kotlin/com/mcstarrysky/aiyatsbus/module/custom/splendid/mechanism/entry/object/ObjectString.kt
@@ -1,20 +1,8 @@
 package com.mcstarrysky.aiyatsbus.module.custom.splendid.mechanism.entry.`object`
 
 import com.mcstarrysky.aiyatsbus.module.custom.splendid.mechanism.entry.internal.ObjectEntry
-import com.mcstarrysky.aiyatsbus.module.custom.splendid.mechanism.entry.internal.objString
 
 object ObjectString : ObjectEntry<String>() {
-
-    override fun get(from: String, objName: String): Pair<ObjectEntry<*>, Any?> {
-        return when {
-            objName.startsWith("分割包含") -> {
-                val parts = objName.removePrefix("分割包含(").removeSuffix(")").split(";")
-                println(parts)
-                return objString to from.split(parts[0]).contains(parts[1])
-            }
-            else -> super.get(from, objName)
-        }
-    }
 
     override fun holderize(obj: String) = this to obj
     override fun disholderize(holder: String) = holder
