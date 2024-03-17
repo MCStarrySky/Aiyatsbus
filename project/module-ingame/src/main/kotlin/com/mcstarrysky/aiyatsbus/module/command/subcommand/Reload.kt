efd0102839a232bd3ac9eb3538d93a1cad8b4b79
Micalhl <micalhl@sina.cn>
2024-03-17 17:11:41 +0800

    Update

--- project/module-ingame/src/main/kotlin/com/mcstarrysky/aiyatsbus/module/command/subcommand/Reload.kt
+++ project/module-ingame/src/main/kotlin/com/mcstarrysky/aiyatsbus/module/command/subcommand/Reload.kt
@@ -1,12 +1,16 @@
 package com.mcstarrysky.aiyatsbus.module.command.subcommand
 
 import com.mcstarrysky.aiyatsbus.core.AIYATSBUS_PREFIX
+import com.mcstarrysky.aiyatsbus.core.Aiyatsbus
+import com.mcstarrysky.aiyatsbus.core.registration.modern.ModernEnchantmentRegisterer
 import com.mcstarrysky.aiyatsbus.core.util.Reloadables
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import taboolib.common.platform.command.subCommand
 import taboolib.module.chat.colored
 import taboolib.module.configuration.ConfigLoader
+import taboolib.module.nms.MinecraftVersion
+import taboolib.module.nms.nmsProxy
 import taboolib.platform.util.onlinePlayers
 
 /**
@@ -18,13 +22,9 @@ import taboolib.platform.util.onlinePlayers
  */
 val reloadSubCommand = subCommand {
     execute<CommandSender> { sender, _, _ ->
-        // AiyatsbusSettings.conf.reload()
-        // AiyatsbusDisplayManager.conf.reload()
-        // AnvilListener.conf.reload()
-        // AttainListener.conf.reload()
-        // ExpListener.conf.reload()
-        // GrindstoneListener.conf.reload()
-        // VillagerListener.conf.reload()
+        if (MinecraftVersion.majorLegacy >= 12003) {
+            (Aiyatsbus.api().getEnchantmentRegisterer() as ModernEnchantmentRegisterer).replaceRegistry()
+        }
         ConfigLoader.files.values.forEach { it.configuration.reload() }
         Reloadables.execute()
         onlinePlayers.forEach(Player::updateInventory)
