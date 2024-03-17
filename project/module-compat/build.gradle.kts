91d472fc537eaa77e4b2b120e8bd8f36230c34ed
Micalhl <micalhl@sina.cn>
2024-03-17 12:57:42 +0800

    Update

--- project/module-compat/build.gradle.kts
+++ project/module-compat/build.gradle.kts
@@ -3,11 +3,10 @@ dependencies {
     compileOnly(project(":project:common"))
     compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
 
-    compileOnly("public:TrChat:2.0.4")
-    compileOnly("public:InteractiveChat:4.2.7.2")
-
     compileOnly("paper:v12004:12004:core")
     compileOnly("ink.ptms.core:v12001:12001:mapped")
+    compileOnly("ink.ptms.core:v11802:11802:mapped")
+    compileOnly("ink.ptms.core:v11802:11802:universal")
 }
 
 // 子模块
--- project/module-compat/src/main/kotlin/com/mcstarrysky/aiyatsbus/module/compat/chat/PacketSystemChat.kt
+++ project/module-compat/src/main/kotlin/com/mcstarrysky/aiyatsbus/module/compat/chat/PacketSystemChat.kt
@@ -1,4 +1,4 @@
-package com.mcstarrysky.aiyatsbus.compat.vanilla
+package com.mcstarrysky.aiyatsbus.module.compat.chat
 
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
@@ -10,9 +10,10 @@ import org.bukkit.entity.Player
 import taboolib.common.env.RuntimeDependency
 import taboolib.common.platform.event.EventPriority
 import taboolib.common.platform.event.SubscribeEvent
-import taboolib.common.platform.function.info
 import taboolib.library.reflex.Reflex.Companion.getProperty
 import taboolib.library.reflex.Reflex.Companion.setProperty
+import taboolib.module.configuration.Configuration
+import taboolib.module.configuration.Type
 import taboolib.module.nms.MinecraftVersion
 import taboolib.module.nms.PacketSendEvent
 
@@ -30,28 +31,42 @@ object PacketSystemChat {
 
     @SubscribeEvent(priority = EventPriority.MONITOR)
     fun e(e: PacketSendEvent) {
+        val player = e.player
+        // 1.19 +
         if (e.packet.name == "ClientboundSystemChatPacket") {
             runCatching {
-                val player = e.player
+                // 1.20.2 + 取消了 adventure$content, 所以要直接修改 IChatBaseComponent
+                // FIXME: 小张看这里有一个 a/adventure 指向 Adventure Component, 但我怎么都测试不出来这个字段
                 if (MinecraftVersion.majorLegacy > 12002) {
-                    info(e.packet.source.toString())
                     val adventure = Aiyatsbus.api().getMinecraftAPI().iChatBaseComponentToComponent(e.packet.source.getProperty<Any>("content") ?: return)
-                    e.packet.source.setProperty("content", Aiyatsbus.api().getMinecraftAPI().componentToIChatBaseComponent(modify(adventure, player)))
+                    e.packet.source.setProperty("content", Aiyatsbus.api().getMinecraftAPI().componentToIChatBaseComponent(
+                        modify(adventure, player)
+                    ))
                 } else {
+                    // 1.19 - 1.20.2 是有一个 adventure$content 存储 Adventure Component, 直接修改这个字段即可
                     val adventure = e.packet.source.getProperty<Any>("adventure\$content", remap = false) as? Component ?: return
                     e.packet.source.setProperty("adventure\$content", modify(adventure, player), remap = false)
                 }
             }.onFailure { it.printStackTrace() }
         }
-        //if (e.packet.name == "PacketPlayInChat") {
-        //    if (MinecraftVersion.isUniversal) {
-        //        info(e.packet.read("message"))
-        //    } else info(e.packet.read("a"))
-        //}
+        // 1.16 - 1.18 的数据包与高版本不同, 要修改 PacketPlayOutChat 的 message, message 是 IChatBaseComponent
+        else
+        if (e.packet.name == "PacketPlayOutChat") {
+            runCatching {
+                val adventure = Aiyatsbus.api().getMinecraftAPI().iChatBaseComponentToComponent(e.packet.source.getProperty<Any>("message") ?: return)
+                e.packet.source.setProperty("message", Aiyatsbus.api().getMinecraftAPI().componentToIChatBaseComponent(
+                    modify(adventure, player)
+                ))
+            }.onFailure { it.printStackTrace() }
+        }
     }
 
-    private fun modify(component: Component, player: Player): Component {
+    fun modify(component: Component, player: Player): Component {
         var json = gson.serialize(component)
+
+        // 弱者做法: 二次解析, 防止 GsonComponentSerializer 把单引号解析成 \u0027
+        json = Configuration.loadFromString(json, Type.FAST_JSON).saveToString()
+
         val stacks = extractHoverEvents(json)
 
         stacks.forEach { stack ->
