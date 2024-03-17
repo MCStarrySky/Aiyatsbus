b1b73334c8d9a363adb9f51a5ca72afcee13e247
Micalhl <micalhl@sina.cn>
2024-03-17 12:57:42 +0800

    Update

---
+++ project/module-compat/src/main/kotlin/com/mcstarrysky/aiyatsbus/module/compat/chat/ListenerPlayerDeath.kt
@@ -0,0 +1,20 @@
+package com.mcstarrysky.aiyatsbus.module.compat.chat
+
+import org.bukkit.event.entity.PlayerDeathEvent
+import taboolib.common.platform.event.SubscribeEvent
+
+/**
+ * Aiyatsbus
+ * com.mcstarrysky.aiyatsbus.compat.vanilla.ListenerPlayerDeath
+ *
+ * @author mical
+ * @since 2024/3/17 12:25
+ */
+object ListenerPlayerDeath {
+
+    @SubscribeEvent
+    fun e(e: PlayerDeathEvent) {
+        // 支持死亡界面显示那个物品
+        e.deathMessage(PacketSystemChat.modify(e.deathMessage() ?: return, e.entity))
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,12 +7,12 @@ include("project:common-impl")
 // 1.20.4 自定义附魔注册器
 include("project:common-impl-v12004-paper")
 include("project:common-impl-v12004-vanilla")
-// 挂钩聊天展示
-include("project:compat-chat")
 // nms 实现
 include("project:common-impl-nms")
 // 运行平台
 include("project:module-bukkit")
+// 与其他插件兼容模块
+include("project:module-compat")
 // 玩家交互代码
 include("project:module-ingame")
 // 脚本
