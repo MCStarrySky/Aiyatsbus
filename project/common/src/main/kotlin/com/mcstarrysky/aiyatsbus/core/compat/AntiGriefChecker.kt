543b345202609ccc61f488fa090a5c2dc09054fa
Daniel Zhang <xiaozhangshiw@163.com>
2024-04-05 01:25:28 +0800

    0.3.0 Fix display and UI & improve & update codestyle

--- project/common/src/main/kotlin/com/mcstarrysky/aiyatsbus/core/compat/AntiGriefChecker.kt
+++ project/common/src/main/kotlin/com/mcstarrysky/aiyatsbus/core/compat/AntiGriefChecker.kt
@@ -1,5 +1,6 @@
 package com.mcstarrysky.aiyatsbus.core.compat
 
+import com.mcstarrysky.aiyatsbus.core.AiyatsbusSettings
 import org.bukkit.Location
 import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
@@ -16,10 +17,12 @@ object AntiGriefChecker {
     private val checkers = hashSetOf<AntiGrief>()
 
     fun canBreak(player: Player, location: Location): Boolean {
+        if (player.isOp && AiyatsbusSettings.antiGriefIgnoreOp) return true
         return checkers.filter(AntiGrief::checkRunning).all { it.canBreak(player, location) }
     }
 
     fun canDamage(player: Player, entity: Entity): Boolean {
+        if (player.isOp && AiyatsbusSettings.antiGriefIgnoreOp) return true
         return checkers.filter(AntiGrief::checkRunning).all { it.canDamage(player, entity) }
     }
 
--- project/module-kether/src/main/kotlin/com/mcstarrysky/aiyatsbus/module/kether/action/ActionCooldown.kt
+++ project/module-kether/src/main/kotlin/com/mcstarrysky/aiyatsbus/module/kether/action/ActionCooldown.kt
@@ -3,11 +3,12 @@ package com.mcstarrysky.aiyatsbus.module.kether.action
 import com.mcstarrysky.aiyatsbus.core.AiyatsbusEnchantment
 import com.mcstarrysky.aiyatsbus.core.AiyatsbusSettings
 import com.mcstarrysky.aiyatsbus.core.asLang
-import com.mcstarrysky.aiyatsbus.core.sendLang
 import com.mcstarrysky.aiyatsbus.core.util.addCd
 import com.mcstarrysky.aiyatsbus.core.util.checkCd
+import com.mcstarrysky.aiyatsbus.core.util.coerceBoolean
 import com.mcstarrysky.aiyatsbus.module.kether.AiyatsbusParser
 import com.mcstarrysky.aiyatsbus.module.kether.aiyatsbus
+import taboolib.common.platform.function.adaptPlayer
 import taboolib.module.chat.component
 import taboolib.module.nms.sendRawActionBar
 
@@ -21,23 +22,24 @@ import taboolib.module.nms.sendRawActionBar
 object ActionCooldown {
 
     /**
-     * a-cd check &enchant &player &sec true/false
+     * a-cd check &enchant &player &sec true/false true/false
+     * 第一个布尔值(必写): 是否通报给玩家
+     * 第二个布尔值(可选): 是否以动作栏形式发送给玩家
      * a-cd add &enchant &player
      */
     @AiyatsbusParser(["a-cd"])
     fun aCdCheck() = aiyatsbus {
         when (nextToken()) {
             "check" -> {
-                combine(any(), player(), double(), bool(true)) { ench, player, sec, send ->
+                combine(any(), player(), double(), bool(true), optional("action-bar", then = bool())) { ench, player, sec, send, actionBar ->
                     return@combine player.checkCd((ench as AiyatsbusEnchantment).basicData.id, sec).let {
                         if (!it.first && send) {
                             val message = player.asLang("messages-misc-cool_down", it.second to "second")
-                                .component().buildColored().toRawMessage()
-                            if (AiyatsbusSettings.coolDownInActionBar) {
-                                player.sendRawActionBar(message)
-                            }
-                            else {
-                                player.sendRawMessage(message)
+                                .component().buildColored()
+                            if (actionBar.coerceBoolean(AiyatsbusSettings.coolDownInActionBar)) {
+                                player.sendRawActionBar(message.toRawMessage())
+                            } else {
+                                message.sendTo(adaptPlayer(player))
                             }
                         }
                         it.first
