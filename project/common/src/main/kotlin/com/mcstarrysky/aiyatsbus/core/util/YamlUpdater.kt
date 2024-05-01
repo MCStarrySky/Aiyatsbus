7bd0633893297f463165cd0e143db43b3c31e3d7
Micalhl <micalhl@sina.cn>
2024-05-01 22:30:27 +0800

    0.5.14 Improve IO

--- project/common/src/main/kotlin/com/mcstarrysky/aiyatsbus/core/util/YamlUpdater.kt
+++ project/common/src/main/kotlin/com/mcstarrysky/aiyatsbus/core/util/YamlUpdater.kt
@@ -16,7 +16,7 @@ import taboolib.module.configuration.Configuration
  */
 object YamlUpdater {
 
-    fun loadFromFile(path: String, autoUpdate: Boolean = true, updateNodes: List<String> = emptyList(), updateExists: Boolean = true): Configuration {
+    fun loadFromFile(path: String, autoUpdate: Boolean = true, updateNodes: List<String> = emptyList()): Configuration {
         // 如果配置不存在, 直接释放即可, 并不需要任何检查操作
         if (!newFile(getDataFolder(), path, create = false).exists()) {
             return Configuration.loadFromFile(releaseResourceFile(path))
@@ -24,23 +24,19 @@ object YamlUpdater {
         val configFile = newFile(getDataFolder(), path)
         val config = YamlConfiguration.loadConfiguration(configFile)
 
-        // 如果不需要自动更新, 直接返回
-        if (!autoUpdate) Configuration.loadFromFile(releaseResourceFile(path))
-
         // 读取 Jar 包内的对应配置文件
         val cache = Configuration.loadFromInputStream(javaClass.classLoader.getResourceAsStream(path) ?: error("resource not found: $path"))
 
         val updated = mutableListOf<String>()
-        read(cache, config, updateNodes, updated, updateExists)
+        read(cache, config, updateNodes, updated, autoUpdate)
         if (updated.isNotEmpty()) {
             config.save(configFile)
-            return Configuration.loadFromFile(configFile)
         }
 
         return Configuration.loadFromOther(config)
     }
 
-    private fun read(cache: ConfigurationSection, to: org.bukkit.configuration.ConfigurationSection, updateNodes: List<String>, updated: MutableList<String>, updateExists: Boolean) {
+    private fun read(cache: ConfigurationSection, to: org.bukkit.configuration.ConfigurationSection, updateNodes: List<String>, updated: MutableList<String>, autoUpdate: Boolean) {
         for (key in cache.getKeys(true)) {
             val root = key.split(".").first()
             if (root !in updateNodes && key !in updateNodes) {
@@ -54,7 +50,7 @@ object YamlUpdater {
             }
 
             // 是否不更新已存在配置, 只补全缺失项
-            if (!updateExists) continue
+            if (!autoUpdate) continue
 
             if (cache[key] == null) {
                 updated += "$key (${to[key]} -> null)"
