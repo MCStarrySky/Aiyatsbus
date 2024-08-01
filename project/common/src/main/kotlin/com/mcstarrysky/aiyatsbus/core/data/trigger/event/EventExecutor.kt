d652ee2ef96be0f1d28f9e33a8ac50e47e4370fd
Micalhl <micalhl@sina.cn>
2024-08-02 01:19:21 +0800

    [0.7][dev] 修复事件映射优先级除 HIGHEST 外其他无法注册问题, 调整事件映射配置

--- project/common/src/main/kotlin/com/mcstarrysky/aiyatsbus/core/data/trigger/event/EventExecutor.kt
+++ project/common/src/main/kotlin/com/mcstarrysky/aiyatsbus/core/data/trigger/event/EventExecutor.kt
@@ -3,7 +3,6 @@ package com.mcstarrysky.aiyatsbus.core.data.trigger.event
 import com.mcstarrysky.aiyatsbus.core.Aiyatsbus
 import com.mcstarrysky.aiyatsbus.core.AiyatsbusEnchantment
 import com.mcstarrysky.aiyatsbus.core.AiyatsbusSettings
-import taboolib.common.platform.event.EventPriority
 import taboolib.common.platform.function.warning
 import taboolib.library.configuration.ConfigurationSection
 import taboolib.library.kether.LocalizedException
@@ -19,8 +18,7 @@ data class EventExecutor @JvmOverloads constructor(
     private val root: ConfigurationSection,
     private val enchant: AiyatsbusEnchantment,
     val listen: String = root.getString("listen")!!,
-    val handle: String = root.getString("handle") ?: "",
-    val priority: EventPriority = EventPriority.valueOf(root.getString("priority") ?: "HIGHEST"),
+    val handle: String = root.getString("handle") ?: ""
 ) {
 
     init {
